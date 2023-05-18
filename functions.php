3298b379a30f887320cdfc43ec0bafa90a780d8c
Celine CG <70968184+CelinaCG@users.noreply.github.com>
2023-05-18 20:26:24 +0200

    Ajout footer dans function register_nav_menus

--- functions.php
+++ functions.php
@@ -6,11 +6,13 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css');
 }
 
-function register_my_menus() {
-    register_my_menus(
+function register_nav_menus() {
+    register_nav_menus(
         array(
-            'header-menu' => __('Header Menu')
+            'header-menu' => __('Header Menu'),
+            'footer-menu' => __('Footer Menu')
         )
     );
 }
-add_action('init', 'register_my_menus');
+add_action('init', 'register_nav_menus');
+add_theme_support( 'custom-logo' );
